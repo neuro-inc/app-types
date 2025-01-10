b7628baaef7ba4340d247d0eb933027948c7f851
costefan <konstantine.ovchynnikov@apolo.us>
2025-01-10 12:57:18 +0000

    added types to llm

--- src/apolo_app_types/common/preset.py
+++ src/apolo_app_types/common/preset.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel, Field
 
 class Preset(BaseModel):
     name: str = Field(..., description="The name of the preset.", title="Preset name")
+    class Config:
+        schema_extra = {
+            "description": "Component deployment preset.",
+        }
