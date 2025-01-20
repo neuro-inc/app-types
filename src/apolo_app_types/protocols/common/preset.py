f2ae32ee931213a9cfd8653aacedaa3661523a84
costefan <konstantine.ovchynnikov@apolo.us>
2025-01-20 18:07:49 +0000

    removed config from preset

--- src/apolo_app_types/protocols/common/preset.py
+++ src/apolo_app_types/protocols/common/preset.py
@@ -3,8 +3,3 @@ from pydantic import BaseModel, Field
 
 class Preset(BaseModel):
     name: str = Field(..., description="The name of the preset.", title="Preset name")
-
-    class Config:
-        json_schema_extra = {
-            "description": "Component deployment preset.",
-        }
