2e0d1a7ca0508a611b79d87787aaa9bdc986994a
costefan <konstantine.ovchynnikov@apolo.us>
2025-02-19 15:45:52 +0000

    finished outputs from postgres

--- tests/unit/test_postgres_outputs_generator.py
+++ tests/unit/test_postgres_outputs_generator.py
@@ -4,18 +4,8 @@ from apolo_app_types.outputs.postgres import get_postgres_outputs
 
 
 @pytest.mark.asyncio
-async def test_sd(setup_clients, mock_kubernetes_client):
+async def test_postgres_outputs(setup_clients, mock_kubernetes_client):
     res = await get_postgres_outputs(
         helm_values={}
     )
-
     assert res
-    assert res["external_api"]["host"] == "example.com"
-    assert res["external_api"]["base_path"] == "/sdapi/v1"
-    assert res["internal_api"]["host"] == "app.default-namespace"
-    assert res["internal_api"]["base_path"] == "/sdapi/v1"
-    assert res["hf_model"] == {
-        "modelHFName": "SD-Model",
-        "modelFiles": "some_file",
-        "hfToken": None,
-    }
