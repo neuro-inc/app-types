1ba456f0acb522d001f097314111c9ec7e0ba8d9
jordyantunes <jordy.antunes@hotmail.com>
2025-02-19 11:08:53 -0300

    update description for registry_url in DockerHubModel

--- src/apolo_app_types/protocols/dockerhub.py
+++ src/apolo_app_types/protocols/dockerhub.py
@@ -6,7 +6,7 @@ from apolo_app_types.protocols.common import AppInputs, AppOutputsV2
 class DockerHubModel(BaseModel):
     registry_url: str = Field(  # noqa: N815
         default="https://index.docker.io/v1/",
-        description="The URL of the registry where the model is stored.",
+        description="The URL of the registry where the container images are stored.",
         title="Registry URL",
     )
     username: str = Field(  # noqa: N815
