5c027b706fb3943b089d41f7f26a70f9aea35bc8
costefan <konstantine.ovchynnikov@apolo.us>
2025-02-20 13:52:15 +0000

    linter fix

--- src/apolo_app_types/protocols/common/validator.py
+++ src/apolo_app_types/protocols/common/validator.py
@@ -6,7 +6,9 @@ from pydantic import BaseModel
 def validate_complex_type_prop(cls: type[BaseModel]) -> None:
     for field_name, field_type in get_type_hints(cls).items():
         if not (isinstance(field_type, type) and issubclass(field_type, BaseModel)):
-            raise TypeError(
-                f"Field '{field_name}' in {cls.__name__} must be a subclass of Pydantic BaseModel, "
+            err_msg = (
+                f"Field '{field_name}' in {cls.__name__} "
+                f"must be a subclass of Pydantic BaseModel, "
                 f"but got {field_type}"
             )
+            raise TypeError(err_msg)
