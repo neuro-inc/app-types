e3a1ab4e7466c2045f14f851605ff4e02c1db0c5
costefan <konstantine.ovchynnikov@apolo.us>
2025-02-20 14:22:16 +0000

    fix tests

--- src/apolo_app_types/protocols/common/inputs.py
+++ src/apolo_app_types/protocols/common/inputs.py
@@ -8,12 +8,6 @@ from apolo_app_types.protocols.common.validator import validate_complex_type_pro
 class AppInputs(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True, protected_namespaces=())
 
-    def __init_subclass__(cls: type["AppInputs"], **kwargs: Unpack[ConfigDict]) -> None:
-        """Validate field types at class definition time."""
-        super().__init_subclass__()
-
-        validate_complex_type_prop(cls)
-
 
 class AppInputsV2(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True, protected_namespaces=())
