71683f63506bd68d5a28c6e0adca9b6727ad52fc
Jordy Antunes <jordy.antunes@hotmail.com>
2025-04-09 15:56:09 -0300

    Remove unused methods from JupyterChartValueProcessor to streamline code

--- src/apolo_app_types/helm/apps/jupyter.py
+++ src/apolo_app_types/helm/apps/jupyter.py
@@ -36,35 +36,6 @@ class JupyterChartValueProcessor(BaseChartValueProcessor[JupyterAppInputs]):
             *args, **kwargs
         )
 
-    async def gen_extra_helm_args(self, *_: t.Any) -> list[str]:
-        return ["--timeout", "30m"]
-
-    async def _configure_env(
-        self, data_volume: URL, outputs_volume: URL
-    ) -> dict[str, str]:
-        return {
-            "CMDARGS": "--listen",
-            "DATADIR": str(data_volume),
-            "config_path": str(data_volume / "config.txt"),
-            "config_example_path": str(
-                data_volume / "config_modification_tutorial.txt"
-            ),
-            "path_checkpoints": str(data_volume / "models/checkpoints/"),
-            "path_loras": str(
-                data_volume / "models/loras/",
-            ),
-            "path_embeddings": str(data_volume / "models/embeddings/"),
-            "path_vae_approx": str(data_volume / "models/vae_approx/"),
-            "path_upscale_models": str(data_volume / "models/upscale_models/"),
-            "path_inpaint": str(data_volume / "models/inpaint/"),
-            "path_controlnet": str(data_volume / "models/controlnet/"),
-            "path_clip_vision": str(data_volume / "models/clip_vision/"),
-            "path_fooocus_expansion": str(
-                data_volume / "models/prompt_expansion/fooocus_expansion/"
-            ),
-            "path_outputs": str(outputs_volume),
-        }
-
     async def gen_extra_values(
         self,
         input_: JupyterAppInputs,
