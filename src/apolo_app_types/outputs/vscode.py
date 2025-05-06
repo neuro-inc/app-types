92b4545c51cf8cffbf6f4db9d6191bebb71f14c4
Jordy Antunes <jordy.antunes@hotmail.com>
2025-05-06 09:23:14 -0300

    Refactor get_vscode_outputs to use get_internal_external_web_urls for URL generation

--- src/apolo_app_types/outputs/vscode.py
+++ src/apolo_app_types/outputs/vscode.py
@@ -2,9 +2,7 @@ import logging
 import typing as t
 
 from apolo_app_types import VSCodeAppOutputs
-from apolo_app_types.clients.kube import get_service_host_port
-from apolo_app_types.outputs.utils.ingress import get_ingress_host_port
-from apolo_app_types.protocols.common.networking import RestAPI
+from apolo_app_types.outputs.common import get_internal_external_web_urls
 
 
 logger = logging.getLogger()
@@ -15,26 +13,9 @@ async def get_vscode_outputs(
 ) -> dict[str, t.Any]:
     if not labels:
         labels = {"application": "vscode"}
-    internal_host, internal_port = await get_service_host_port(match_labels=labels)
-    internal_web_app_url = None
-    if internal_host:
-        internal_web_app_url = RestAPI(
-            host=internal_host,
-            port=int(internal_port),
-            base_path="/",
-            protocol="http",
-        )
-
-    host_port = await get_ingress_host_port(match_labels=labels)
-    external_web_app_url = None
-    if host_port:
-        host, port = host_port
-        external_web_app_url = RestAPI(
-            host=host,
-            port=int(port),
-            base_path="/",
-            protocol="https",
-        )
+    internal_web_app_url, external_web_app_url = await get_internal_external_web_urls(
+        labels
+    )
     outputs = VSCodeAppOutputs(
         internal_web_app_url=internal_web_app_url,
         external_web_app_url=external_web_app_url,
