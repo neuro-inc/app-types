e87d1c127af2db62bcf48c3954eab6efbffa5fb9
Yevhenii Semendiak <semendyak@gmail.com>
2025-05-20 11:39:48 +0000

    MLO-260: PostgreSQL: add all user <> database combinations to outputs (#118)
    * MLO-260: PostgreSQL: add all user <> database combinations to outputs
    
    * bit more assertions

--- tests/unit/postgres/test_postgres_outputs_generator.py
+++ tests/unit/postgres/test_postgres_outputs_generator.py
@@ -1,9 +1,6 @@
-import pytest
-
 from apolo_app_types.outputs.postgres import get_postgres_outputs
 
 
-@pytest.mark.asyncio
 async def test_postgres_outputs(setup_clients, mock_kubernetes_client):
     res = await get_postgres_outputs(helm_values={})
     assert res["postgres_users"]["users"] == [
@@ -22,5 +19,28 @@ async def test_postgres_outputs(setup_clients, mock_kubernetes_client):
             "postgres_uri": {
                 "uri": "postgresql://admin:supersecret@pgbouncer.example.com:6432/mydatabase"
             },
-        }
+        },
+        {
+            "dbname": "otherdatabase",
+            "user": "admin",
+            "password": "supersecret",
+            "host": "db.example.com",
+            "port": 5432,
+            "pgbouncer_host": "pgbouncer.example.com",
+            "pgbouncer_port": 6432,
+            "jdbc_uri": "jdbc:postgresql://db.example.com:5432/otherdatabase",
+            "pgbouncer_jdbc_uri": "jdbc:postgresql://pgbouncer.example.com:6432/otherdatabase",
+            "pgbouncer_uri": "postgres://pgbouncer.example.com:6432/otherdatabase",
+            "uri": "postgres://db.example.com:5432/otherdatabase",
+            "postgres_uri": {
+                "uri": "postgresql://admin:supersecret@pgbouncer.example.com:6432/otherdatabase"
+            },
+        },
     ]
+    mock = mock_kubernetes_client["mock_custom_objects"]
+    mock.list_namespaced_custom_object.assert_called_once_with(
+        group="postgres-operator.crunchydata.com",
+        version="v1beta1",
+        namespace="default-namespace",
+        plural="postgresclusters",
+    )
