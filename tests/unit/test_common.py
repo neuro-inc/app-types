515f91c66e2079661239c18d9e12fd7e48a49ab2
Paul Klym <paul.klymenko@apolo.us>
2025-05-27 10:39:28 +0200

    Improve error message match in preset not found test

--- tests/unit/test_common.py
+++ tests/unit/test_common.py
@@ -10,7 +10,7 @@ def test_get_preset_raises_value_error_when_preset_not_found():
     client.config.cluster_name = "test-cluster"
     client.config.presets = {"gpu-small": MagicMock()}
 
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(
+        ValueError, match="Preset invalid not exist in cluster test-cluster"
+    ):
         get_preset(client, "invalid")
-
-    assert str(exc_info.value) == "Preset invalid not exist in cluster test-cluster"
