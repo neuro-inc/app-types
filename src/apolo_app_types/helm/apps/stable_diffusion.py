817df63174f0d14a731847f5b9460572e986793f
Ovchinnikov Konstantyn <costefan@users.noreply.github.com>
2025-06-02 19:15:34 +0100

    fix stable diffusion (#153)
    * fix stable diffusion
    
    * fix mypy

--- src/apolo_app_types/helm/apps/stable_diffusion.py
+++ src/apolo_app_types/helm/apps/stable_diffusion.py
@@ -10,12 +10,15 @@ from apolo_app_types.helm.apps.common import (
     get_preset,
 )
 from apolo_app_types.helm.utils.deep_merging import merge_list_of_dicts
+from apolo_app_types.protocols.common.secrets_ import serialize_optional_secret
 
 
 class StableDiffusionChartValueProcessor(
     BaseChartValueProcessor[StableDiffusionInputs]
 ):
-    def _get_env_vars(self, preset: Preset) -> dict[str, t.Any]:
+    def _get_env_vars(
+        self, input_: StableDiffusionInputs, preset: Preset, app_secrets_name: str
+    ) -> dict[str, t.Any]:
         default_cmd_args = "--docs --cors-origins=*"
         if preset.nvidia_gpu:
             commandline_args = "--use-cuda"
@@ -23,8 +26,13 @@ class StableDiffusionChartValueProcessor(
             commandline_args = "--use-rocm"
         else:
             commandline_args = "--lowvram"
-
-        return {"COMMANDLINE_ARGS": " ".join([default_cmd_args, commandline_args])}
+        return {
+            "COMMANDLINE_ARGS": " ".join([default_cmd_args, commandline_args]),
+            "HUGGING_FACE_HUB_TOKEN": serialize_optional_secret(
+                input_.stable_diffusion.hugging_face_model.hf_token,
+                secret_name=app_secrets_name,
+            ),
+        }
 
     def _get_image_repository(self, preset: Preset) -> str:
         if preset.nvidia_gpu:
@@ -41,6 +49,7 @@ class StableDiffusionChartValueProcessor(
         input_: StableDiffusionInputs,
         app_name: str,
         namespace: str,
+        app_secrets_name: str,
         *_: t.Any,
         **kwargs: t.Any,
     ) -> dict[str, t.Any]:
@@ -52,7 +61,7 @@ class StableDiffusionChartValueProcessor(
         preset = get_preset(self.client, preset_name)
 
         component_vals = get_component_values(preset, preset_name)
-        api_vars = self._get_env_vars(preset)
+        api_vars = self._get_env_vars(input_, preset, app_secrets_name)
         img_repository = self._get_image_repository(preset)
 
         model_vals = {
