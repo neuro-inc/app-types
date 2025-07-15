4b5831e27b424754f06e9e05510a90d23a3192fd
Yevhenii Semendiak <semendyak@gmail.com>
2025-07-15 17:16:15 +0300

    Populate app ID to BaseAppOutputsProcessor.generate_outputs

--- src/apolo_app_types/outputs/base.py
+++ src/apolo_app_types/outputs/base.py
@@ -14,13 +14,15 @@ class BaseAppOutputsProcessor(abc.ABC, t.Generic[AppOutputsT]):
     async def generate_outputs(
         self,
         helm_values: dict[str, t.Any],
+        app_instance_id: str,
     ) -> dict[str, t.Any]:
         msg = f"Generating outputs for {helm_values}"
         logger.info(msg)
-        return (await self._generate_outputs(helm_values)).model_dump()
+        return (await self._generate_outputs(helm_values, app_instance_id)).model_dump()
 
     @abc.abstractmethod
     async def _generate_outputs(
         self,
         helm_values: dict[str, t.Any],
+        app_instance_id: str,
     ) -> AppOutputsT: ...
