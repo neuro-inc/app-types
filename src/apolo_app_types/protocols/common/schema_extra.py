cb90075bf703231e8f2182fdeb983c9b4c1ed457
Yevhenii Semendiak <semendyak@gmail.com>
2025-11-21 16:49:49 +0200

    MLO-507: Add x-is-configurable extra schema field to mark parameter that is not configurable (#384)

--- src/apolo_app_types/protocols/common/schema_extra.py
+++ src/apolo_app_types/protocols/common/schema_extra.py
@@ -17,6 +17,7 @@ class SchemaExtraMetadata(BaseModel):
     logo_url: str | None = Field(None, alias="x-logo-url")
     # If field can be collapsed in UI (more advanced setup)
     is_advanced_field: bool = Field(default=False, alias="x-is-advanced-field")
+    is_configurable: bool = Field(default=True, alias="x-is-configurable")
     model_config = {"populate_by_name": True}
 
     def as_json_schema_extra(self) -> dict[str, Any]:
